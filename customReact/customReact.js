b79b809a8798b092fcea76ac1e35637544e5455c
Adarsh <isadarsh02@gmail.com>
2024-05-21 10:58:26 +0530

    customreact

---
+++ customReact/customReact.js
@@ -0,0 +1,24 @@
+
+function customRender(reactElement, container) {
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    const props = reactElement.props;
+    for (key in props) {
+        if (key === 'children') continue
+        domElement.setAttribute(key, props[key])
+    }
+    container.appendChild(domElement)
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://github.com/isadarsh',
+        target: '_blank'
+    },
+    children: 'click me to visit github'
+}
+
+const mainContainer = document.querySelector('#root')
+
+const render=  customRender(reactElement, mainContainer);
