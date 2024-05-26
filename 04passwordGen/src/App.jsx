3f87ba2c5c5306751928c13bc9c2bc40bd8d21e9
Adarsh <isadarsh02@gmail.com>
2024-05-26 16:00:02 +0530

    passweirdGen

--- 04passwordGen/src/App.jsx
+++ 04passwordGen/src/App.jsx
@@ -1,37 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 function App() {
   const [password, setPassword] = useState('raaz-KI-baat-BATAYEIN');
   const [length, setLength] = useState(8);
-  const [numberAllowed, setNumberAllowed] = useState(0);
-  const [characterAllowed, setCharacterAllowed] = useState(0);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [characterAllowed, setCharacterAllowed] = useState(false);
+  //useRefhook
+  const passwordRef= useRef(null)
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ''
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    if (numberAllowed) str += '0123456789'
+    if (characterAllowed) str += '!@#$%^&*()_+'
+
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+    }
+
+    setPassword(pass)
+  }, [length, numberAllowed, characterAllowed, setPassword])//why to add fun (setpassword?) - For sake of optimisation (memoized)
+
+  const copyPasswordToClipboard = () => {
+    window.navigator.clipboard.writeText(password);
+  }
+
+
+  useEffect(() => { passwordGenerator() }, [passwordGenerator, length, numberAllowed, characterAllowed])
 
   return (
-    <>//a input box of 75percent length of div followed by a 25percent (copy) button
+    <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800">
+
         <h1 className='text-center my-31 py-2'>Password Generator</h1>
+
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
           <input type='text' value={password} className='outline-none w-full py-2 px-3 sm:w-3/4 md:w-full'
             placeholder='password' readOnly />
-          <button className='outline-none bg-orange-500 text-white px-3 py-0.5 shrink-0'>
+          <button onClick= {copyPasswordToClipboard}
+            className='outline-none bg-orange-500 text-white px-3 py-0.5 shrink-0 hover:bg-blue-700 active:scale-90'>
             Copy</button>
         </div>
+
         <div className="flex justify-center text-sm gap-x-2 py-2">
+
           <div className="flex items-center gap-x-1">
             <input type="range" name="length of password"
               min={8} max={20} value={length}
               className='cursor-pointer'
-            onChange={(e)=>{setLength(e.target.value)}}/>
+              onChange={(e) => { setLength(e.target.value) }} />
             <label htmlFor="length">Length: {length}</label>
           </div>
+
           <div className="flex items-center gap-x-1">
-            <input type="checkbox" defaultChecked={numberAllowed} id="numberInput"
+            <input ref={passwordRef} type="checkbox" defaultChecked={numberAllowed} id="numberInput"
               onChange={() => { setNumberAllowed((prev) => !prev) }} />
             <label htmlFor="numberAllowed">Numbers</label>
           </div>
+
           <div className="flex items-center gap-x-1">
             <input type="checkbox" defaultChecked={characterAllowed} id="characterInput"
               onChange={() => { setCharacterAllowed((prev) => !prev) }} />
